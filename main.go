6cd814966aaf306d236d530de976767f01767ca1
bassel <b.farhat@pm.me>
2024-03-03 16:10:49 +0100

    Feature: Add get /

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello World!")
+    mux := http.NewServeMux()
+
+    mux.Handle("/", http.FileServer(http.Dir(".")))
+
+    corsMux := middlewareCors(mux)
+
+    fmt.Println("Starting server on port 8080...")
+    server := http.Server{
+        Addr: "localhost:8080",
+        Handler: corsMux,
+    }
+    
+    server.ListenAndServe()
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+        w.Header().Set("Access-Control-Allow-Headers", "*")
+        if r.Method == "OPTIONS" {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+        next.ServeHTTP(w, r)
+    })
 }
