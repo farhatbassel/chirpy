cf73ab8788c279976cbd95c6a0ece27b132acc98
bassel <b.farhat@pm.me>
2024-03-10 18:15:06 +0100

    Feature: Add some endpoints to the chirp app

--- main.go
+++ main.go
@@ -3,14 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-    mux := http.NewServeMux()
+    router := chi.NewRouter()
 
-    mux.Handle("/", http.FileServer(http.Dir(".")))
+    apiConfig := apiConfig{}
 
-    corsMux := middlewareCors(mux)
+    // Static Routes
+    router.Handle("/app/", 
+        apiConfig.middlewareMetricsInc(
+            http.StripPrefix("/app/", http.FileServer(http.Dir("."))),
+        ),
+    )
+    router.Handle("/assets/", 
+        http.StripPrefix("/assets/", http.FileServer(http.Dir("/assets"))),
+    )
+
+    // Admin routes
+    router.Get("/admin/metrics", apiConfig.displayNumberOfHits)
+
+    // API Routes
+    router.HandleFunc("/api/reset", apiConfig.resetNumberOfHits)
+    router.Get("/api/healthz", checkForHealth)
+    router.Post("/api/validate_chirp", apiConfig.validateChirp)
+
+    corsMux := middlewareCors(router)
 
     fmt.Println("Starting server on port 8080...")
     server := http.Server{
@@ -21,15 +41,9 @@ func main() {
     server.ListenAndServe()
 }
 
-func middlewareCors(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-        w.Header().Set("Access-Control-Allow-Headers", "*")
-        if r.Method == "OPTIONS" {
-            w.WriteHeader(http.StatusOK)
-            return
-        }
-        next.ServeHTTP(w, r)
-    })
+func checkForHealth(writer http.ResponseWriter, request *http.Request) {
+    writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+    writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(http.StatusText(http.StatusOK)))
 }
+
---
+++ serverHits.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (config *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+        config.fileserverHits += 1
+        next.ServeHTTP(writer, request)
+    })
+}
+
+func (config *apiConfig) displayNumberOfHits(writer http.ResponseWriter, request *http.Request) {
+    writer.Header().Add("Content-Type", "text/html")
+    writer.WriteHeader(http.StatusOK)
+
+    htmlBody := fmt.Sprintf(`
+    <html>
+
+        <body>
+            <h1>Welcome, Chirpy Admin</h1>
+            <p>Chirpy has been visited %d times!</p>
+        </body>
+
+    </html>
+    `, config.fileserverHits)
+
+    writer.Write([]byte(htmlBody))
+}
+
+func (config *apiConfig) resetNumberOfHits(writer http.ResponseWriter, request *http.Request) {
+    writer.WriteHeader(http.StatusOK)
+    config.fileserverHits = 0
+}
+
+func (config *apiConfig) validateChirp(writer http.ResponseWriter, request *http.Request) {
+    type chirp struct {
+        Body string `json:"body"`
+    }
+
+    type chirpResponse struct {
+        Valid bool `json:"valid"`
+        Error string `json:"error"`
+        CleanedBody string `json:"cleaned_body"`
+    }
+
+    decoder := json.NewDecoder(request.Body)
+    params := chirp{}
+
+    if err := decoder.Decode(&params); err != nil {
+        writer.WriteHeader(500)
+        return
+    }
+
+    responseBody := chirpResponse{}
+    if len(params.Body) > 140 {
+        responseBody.Error = "Chirp is too long"
+        sendJSONResponse(writer, responseBody, http.StatusBadRequest)
+        return 
+    } 
+
+    responseBody.CleanedBody = cleanBody(params.Body)
+    responseBody.Valid = true
+    sendJSONResponse(writer, responseBody, http.StatusOK)
+}
+
+func sendJSONResponse(writer http.ResponseWriter, data interface{}, status int) {
+    writer.Header().Set("Content-Type", "application/json")
+    writer.WriteHeader(status)
+
+    if err := json.NewEncoder(writer).Encode(data); err != nil {
+        http.Error(writer, "Failed to encode JSON response", http.StatusInternalServerError)
+    }
+}
+
+func cleanBody(input string) string {
+    words := strings.Fields(input)
+    badWords := map[string]struct{}{
+        "kerfuffle": {},
+        "sharbert": {},
+        "fornax": {},
+    }
+    output := make([]string, 0);
+    for _, word := range words {
+        lowerWord := strings.ToLower(word)
+        if _, isBadWord := badWords[lowerWord]; isBadWord {
+            output = append(output, "****")
+        } else { 
+            output = append(output, word)
+        }
+    }
+
+    return strings.Join(output, " ")
+}
